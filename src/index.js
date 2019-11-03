fe156d8ec902950f4f7771789d14189a11683e17
TheCodeNinja <hk4public@gmail.com>
2019-11-03 22:33:47 +0800

    Install 'react-router-dom' and create routes

--- src/index.js
+++ src/index.js
@@ -2,4 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Button } from 'antd-mobile'
 
-ReactDOM.render(<Button type="primary">Test</Button>, document.getElementById('root'))
+import { HashRouter, Route, Switch } from 'react-router-dom'
+
+import Register from './containers/register/register'
+import Login from './containers/login/login'
+import Main from './containers/main/main'
+
+
+ReactDOM.render((
+    <HashRouter>
+        <Switch>
+            <Route path='/register' component={Register}></Route>
+            <Route path='/login' component={Login}></Route>
+            <Route component={Main}></Route>
+        </Switch>
+    </HashRouter>
+), document.getElementById('root'))
