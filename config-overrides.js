db05fc8ba30b261e735fef2e085b058d10c244f2
TheCodeNinja <hk4public@gmail.com>
2019-11-04 01:49:16 +0800

    Create static components for register page

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,16 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
 module.exports = override(
     fixBabelImports('import', {
         libraryName: 'antd-mobile',
-        style: 'css',
+        libraryDirectory: 'es',
+        style: true // change importing css to less
     }),
+    addLessLoader({
+        //javascriptEnabled: true,
+        modifyVars: { 
+            "@brand-primary": "#1cae82", 
+            "@brand-primary-tap": "#1DA57A" 
+        }
+    })
 );
--- src/containers/register/register.jsx
+++ src/containers/register/register.jsx
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import {
+    NavBar, WingBlank, List, InputItem, WhiteSpace, Radio, Button
+} from 'antd-mobile'
+
+import Logo from '../../components/logo/logo'
+
+const ListItem = List.Item
 
 export default class Register extends Component {
     render () {
         return (
-            <div>Register</div>
+            <div>
+                <NavBar>硅&nbsp;谷&nbsp;宜&nbsp;聘</NavBar>
+                <Logo />
+                <WingBlank>
+                    <List>
+                        <WhiteSpace />
+                        <InputItem>用戶名：</InputItem>
+                        <WhiteSpace />
+                        <InputItem type="password">密&nbsp;&nbsp;&nbsp;&nbsp;碼：</InputItem>
+                        <WhiteSpace />
+                        <InputItem type="password">確認密碼：</InputItem>
+                        <WhiteSpace />
+                        <ListItem>
+                            <span>用戶類型：</span>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <Radio>求職</Radio>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <Radio>招聘</Radio>
+                        </ListItem>
+                        <WhiteSpace />
+                        <Button type="primary">註&nbsp;册</Button>
+                        <Button>登入</Button>
+                    </List>
+                </WingBlank>
+            </div>
         )
     }
 }
