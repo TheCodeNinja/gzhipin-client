73be6efbf298a52158383be00043931cb3704887
TheCodeNinja <hk4public@gmail.com>
2019-11-04 02:40:49 +0800

    Cope with the form data of register page

--- src/containers/register/register.jsx
+++ src/containers/register/register.jsx
@@ -8,7 +8,33 @@ import Logo from '../../components/logo/logo'
 const ListItem = List.Item
 
 export default class Register extends Component {
+
+    state = {
+        username: '',
+        password: '',
+        password2: '',
+        type: 'recruit'
+    }
+
+    register = () => {
+        console.log(this.state)
+    }
+
+    // 處理輸入數據的改變，更新對應的狀態
+    handleChange = (name, val) => {
+        this.setState({
+            [name]: val // [name]: 取name變量的值
+        })
+    }
+
+    toLogin = () => {
+        this.props.history.replace('/login')
+    }
+
     render () {
+
+        const { type } = this.state
+
         return (
             <div>
                 <NavBar>硅&nbsp;谷&nbsp;宜&nbsp;聘</NavBar>
@@ -16,22 +42,22 @@ export default class Register extends Component {
                 <WingBlank>
                     <List>
                         <WhiteSpace />
-                        <InputItem>用戶名：</InputItem>
+                        <InputItem onChange={val => {this.handleChange('username', val)}}>用戶名：</InputItem>
                         <WhiteSpace />
-                        <InputItem type="password">密&nbsp;&nbsp;&nbsp;&nbsp;碼：</InputItem>
+                        <InputItem type="password" onChange={val => {this.handleChange('password', val)}}>密&nbsp;&nbsp;&nbsp;&nbsp;碼：</InputItem>
                         <WhiteSpace />
-                        <InputItem type="password">確認密碼：</InputItem>
+                        <InputItem type="password" onChange={val => {this.handleChange('password2', val)}}>確認密碼：</InputItem>
                         <WhiteSpace />
                         <ListItem>
                             <span>用戶類型：</span>
                             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            <Radio>求職</Radio>
+                            <Radio checked={type === 'findJob'} onChange={val => {this.handleChange('type', 'findJob')}}>求職</Radio>
                             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            <Radio>招聘</Radio>
+                            <Radio checked={type === 'recruit'} onChange={val => {this.handleChange('type', 'recruit')}}>招聘</Radio>
                         </ListItem>
                         <WhiteSpace />
-                        <Button type="primary">註&nbsp;册</Button>
-                        <Button>登入</Button>
+                        <Button type="primary" onClick={this.register}>註&nbsp;册</Button>
+                        <Button onClick={this.toLogin}>登&nbsp;入</Button>
                     </List>
                 </WingBlank>
             </div>
