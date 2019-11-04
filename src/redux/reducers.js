325a11fcc67df2960fb1a9545eca2d8146b80d47
TheCodeNinja <hk4public@gmail.com>
2019-11-04 18:38:25 +0800

    Define user reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -3,18 +3,33 @@
  */
 
 import { combineReducers } from 'redux'
+import { AUTH_SUCCESS, ERROR_MSG } from './action-types'
 
-function xxx(state=0, action) {
-    return state
+// state的初始对象
+const initUser = {
+    username: '', 
+    type: '', 
+    msg: ''
 }
 
-function yyy(state=0, action) {
-    return state
+function user(state=initUser, action) {
+    switch (action.type) {
+        case AUTH_SUCCESS: // data是user
+            return {
+                ...state, // 先把原本的state对象拆解出来
+                ...action.data // 因为data是user, 拆解他并覆盖掉旧的state对象的值
+            }
+        case ERROR_MSG: // data是msg
+            return {
+                ...state, 
+                msg: action.data
+            }
+        default:
+            return state
+    }
 }
 
-
-// 向外暴露的狀態的結構：{xxx: 0, yyy: 0}
+// 向外暴露的狀態的結構：{user: {}}
 export default combineReducers({
-    xxx,
-    yyy
+    user
 })
