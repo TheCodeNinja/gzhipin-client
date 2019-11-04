b37a87b88c35efc8709753e8450d9fcc842eb9e7
TheCodeNinja <hk4public@gmail.com>
2019-11-04 17:16:22 +0800

    Define different ajax request apis

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+/*
+包含了n個接口請求函數的模塊
+函數返回值是promise
+*/
+
+import ajax from './ajax'
+
+// 定義註册接口
+export const reqRegister = (user) => ajax('/register', user, 'POST')
+
+// 定義登陸接口
+export const reqLogin = ({username, password}) => ajax('/login', {username, password}, 'POST')
+
+// 定義更新用戶接口
+export const reqUpdateUser = (user) => ajax('/update', user, 'POST')
