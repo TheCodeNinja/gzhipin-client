4ab1d5cd0242a2b508df3386d61e64087c2e5c07
TheCodeNinja <hk4public@gmail.com>
2019-11-05 18:13:31 +0800

    lesson-14 - Define static ui for header-selector

--- src/components/header-selector/header-selector.jsx
+++ src/components/header-selector/header-selector.jsx
@@ -1,9 +1,29 @@
+/* 头像选择UI組件 */
+
 import React, { Component } from 'react'
+import { List, Grid } from 'antd-mobile'
 
 export default class HeaderSelctor extends Component {
+
+    // Constructor
+    constructor(props) {
+        super(props);
+        // 准备好数据
+        this.headerList = [];
+        for (let i = 0; i < 20; i++) {
+            this.headerList.push({
+                text: '头像' + (i + 1),
+                icon: require(`../../assets/images/头像${i + 1}.png`) // 不能使用import
+            })
+        }
+    }
+
     render() {
+        const listHeader = '请选择头像'
         return (
-            <div>頭像選擇器</div>
+            <List renderHeader={() => listHeader}>
+                <Grid data={this.headerList} columnNum={5}></Grid>
+            </List>
         )
     }
 }
