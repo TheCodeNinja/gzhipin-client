47ae484027fe12e2a771eeab177d5458991f0f47
TheCodeNinja <hk4public@gmail.com>
2019-11-15 11:01:11 +0800

    lesson-24 - Complete personal component with ui and redux data

--- src/containers/personal/personal.jsx
+++ src/containers/personal/personal.jsx
@@ -4,17 +4,43 @@ Personal ui container route component
 
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Result, Button, List, WhiteSpace, Modal } from 'antd-mobile'
+
+const Item = List.Item;
+const Brief = Item.Brief;
 
 class Personal extends Component {
+
     render() {
+
+        const { 
+            username, info, company, salary, header, post
+        } = this.props.user;
+        
         return (
             <div>
-                Personal
+                <Result
+                    img={<img src={require(`../../assets/images/${header}.png`)} 
+                    style={{width: 50}}/>}
+                    title={username}
+                    message={company}>
+                </Result>
+                <List renderHeader={() => '相关信息'}>
+                    <Item multipleLine>
+                        <Brief>职位：{post}</Brief>
+                        <Brief>简介：{info}</Brief>
+                        {salary ? <Brief>薪资：{salary}</Brief> : null}
+                    </Item>
+                </List>
+                <WhiteSpace></WhiteSpace>
+                <List>
+                    <Button type='warning'>退出登录</Button>
+                </List>
             </div>
         )
     }
 }
  export default connect(
-     state => ({}),
+     state => ({user: state.user}),
      {}
  )(Personal)
