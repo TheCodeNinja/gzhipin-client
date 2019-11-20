c5348111d7101a13e85a4f531f35e5f87435312e
TheCodeNinja <hk4public@gmail.com>
2019-11-20 18:53:14 +0800

    lesson-38 - Create static message component

--- src/containers/message/message.jsx
+++ src/containers/message/message.jsx
@@ -4,13 +4,28 @@ Message ui container route component
 
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { List, Badge } from 'antd-mobile'
+
+const Item = List.Item
+const Brief = Item.Brief
 
 class Message extends Component {
     render() {
         return (
-            <div>
-                Message
-            </div>
+            <List style={{marginTop: 50, marginBottom: 50}}>
+                <Item 
+                    extra={<Badge text={3}/>}
+                    thumb={require(`../../assets/images/头像1.png`)}
+                    arrow='horizontal'>
+                    Hello <Brief>nr1</Brief>
+                </Item>
+                <Item 
+                    extra={<Badge text={0}/>}
+                    thumb={require(`../../assets/images/头像1.png`)}
+                    arrow='horizontal'>
+                    Hello <Brief>nr2</Brief>
+                </Item>
+            </List>
         )
     }
 }
